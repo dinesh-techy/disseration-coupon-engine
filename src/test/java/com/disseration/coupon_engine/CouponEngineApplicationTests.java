67ddf2a6ce77cc3c3e79a673deaa9e55ae15e2a6
dinesh-techy <dineshtechy20@gmail.com>
2025-09-03 22:14:44 +0530

    Initial commit

---
+++ src/test/java/com/disseration/coupon_engine/CouponEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.disseration.coupon_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
